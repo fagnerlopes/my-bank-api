ff4d3f6dd664a370549f05724c38b9ef7c89c270
Fagner Lopes <fagnernlopes@gmail.com>
2021-03-22 13:38:35 -0300

    POST method

--- index.js
+++ index.js
@@ -9,23 +9,20 @@ app.use(express.json());
 
 app.use('/accounts', accountsRouter);
 
-
-
 app.listen(3000, async () => {
-  console.log('API started');
-  
 
   try {
     await readFile('accounts.json');
+    console.log('API started '); 
   } catch (err) {
     const initialJson = {
       nextId: 1,
       accounts: []
     }
-    writeFile('accounts.json', JSON.stringify(initialJson));
-
-    
+    writeFile('accounts.json', JSON.stringify(initialJson)).then(() => {
+      console.log('API started and file created');
+    }).catch(err => {
+      console.log(err); 
+    });    
   }
-  
-
 });
--- routes/account.js
+++ routes/account.js
@@ -1,6 +1,27 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
+import { promises as fs } from "fs";
+const { readFile, writeFile } = fs;
+
+router.post("/", async (req, res) => {
+  try {
+    let account = req.body;
+    const data = JSON.parse(await readFile('accounts.json'));
+    account = { id: data.nextId, ...account };
+    data.nextId++; 
+    data.accounts.push(account);
+
+    await writeFile('accounts.json', JSON.stringify(data, null, 2));
+    
+    res.send(account);
+  } catch (err) {
+    res.status(400).send({error: err.message})
+    
+  }
+  
 
 
+  
+});
 
 export default router;
