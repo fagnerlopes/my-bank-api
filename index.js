cb2681accff5ddd7dc46d5b09cf778d277065843
Fagner Lopes <fagnernlopes@gmail.com>
2021-03-22 15:52:35 -0300

    Routes

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 import express from 'express';
+import accountsRouter from './routes/account.js';
+import { promises as fs } from 'fs';
+
+const { readFile, writeFile } = fs;
+
 const app = express();
 app.use(express.json());
 
+app.use('/accounts', accountsRouter);
 
 
-app.listen(3000, () => {
+
+app.listen(3000, async () => {
   console.log('API started');
+  
+
+  try {
+    await readFile('accounts.json');
+  } catch (err) {
+    const initialJson = {
+      nextId: 1,
+      accounts: []
+    }
+    writeFile('accounts.json', JSON.stringify(initialJson));
+
+    
+  }
+  
+
 });
